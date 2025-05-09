bb0286632964e6b2db409a867d434dfb2c645bf8
jangidRkt08 <itsmejangid.rk@gmail.com>
2025-05-09 18:57:39 +0530

    added Basic folder structure

---
+++ q1/utils/constant.js
@@ -0,0 +1,10 @@
+const URL_MAP = {
+  p: "http://20.244.56.144/evaluation-service/primes",
+  f: "http://20.244.56.144/evaluation-service/fibo",
+  e: "http://20.244.56.144/evaluation-service/even",
+  r: "http://20.244.56.144/evaluation-service/rand",
+};
+
+const WINDOW_SIZE = 10;
+
+module.exports = { URL_MAP, WINDOW_SIZE };
